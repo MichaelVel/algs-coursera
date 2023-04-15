12ff8b7d2b0dc8693b0b03e4ac1320b8acc476f9
sebastian <msvelandiag@unal.edu.co>
2023-04-15 17:48:10 -0500

    final programming exercise: KdTrees

---
+++ kdtrees/src/main/java/mickvel/PointSET.java
@@ -0,0 +1,37 @@
+package mickvel;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+    public PointSET() {
+    }
+
+    public int size() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void insert(Point2D p) {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean contains(Point2D p) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void draw() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        throw new UnsupportedOperationException();
+    }
+
+    public Point2D nearest(Point2D p) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void main(String[] args) {
+    }
+
+}
